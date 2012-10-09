33d4423f305ddb31bf7a9755b0ffed37decd9896
Harry Mills <harry@haeg.in>
2012-10-09 21:36:59 +0100

    Creates rails app and adds vim to gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haste::Application.config.secret_token = '8c6b343fb3fd6274fc5e8d007bf7565325e20708df8faa874cf0c3967f968a35fd0d6246c3799a3edf46e9a04de5e19ae74c67acf8d1b201e40bba1258e7f33f'
